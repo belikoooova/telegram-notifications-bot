6311639b74f6137d3b40fab80097f8c7fa6751d1
Maria Belikova <mebelikova@edu.hse.ru>
2024-02-18 21:51:18 +0300

    delete some tests because of github limits

--- bot/src/test/java/edu/java/bot/SimpleLinkFactoryTest.java
+++ bot/src/test/java/edu/java/bot/SimpleLinkFactoryTest.java
@@ -18,12 +18,6 @@ class SimpleLinkFactoryTest {
     private static final String CORRECT_URL_GH_2 = "https://github.com/belikoooova/telegram-notifications-bot/";
     private static final String INCORRECT_URL_GH_1 = "https://github.com/belikoooova/";
     private static final String INCORRECT_URL_GH_2 = "https://github.com/belikoooova/non-existing-repo";
-    private static final String CORRECT_URL_SO_1 =
-        "https://stackoverflow.com/questions/454396/forum-for-asp-net-mvc-site/454408#454408";
-    private static final String CORRECT_URL_SO_2 =
-        "https://stackoverflow.com/questions/454396";
-    private static final String INCORRECT_URL_SO_1 = "https://stackoverflow.com/questionsS/4";
-    private static final String INCORRECT_URL_SO_2 = "https://stackoverflow.com/questionsS/4";
     private static final long USER_ID = 1;
 
     private SimpleLinkFactory linkFactory;
@@ -52,32 +46,6 @@ class SimpleLinkFactoryTest {
     @ValueSource(strings = {INCORRECT_URL_GH_1, INCORRECT_URL_GH_2})
     @DisplayName("Create link with invalid GitHub URL")
     void testCreateLinkWithInvalidURLGH(String url) {
-        try {
-            Link link = linkFactory.create(USER_ID, url);
-            assertNull(link);
-        } catch (IllegalArgumentException | URISyntaxException e) {
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {CORRECT_URL_SO_1, CORRECT_URL_SO_2})
-    @DisplayName("Create link with valid StackOverflow URL")
-    void testCreateLinkWithValidURLSO(String url) {
-        try {
-            Link link = linkFactory.create(USER_ID, url);
-            assertNotNull(link);
-            assertEquals(url, link.getUrl().toString());
-            assertEquals(LinkResource.STACKOVERFLOW, link.getResource());
-            assertEquals("454396", link.getIdOnResource());
-        } catch (URISyntaxException e) {
-            fail("URISyntaxException should not be thrown for a valid URL");
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {INCORRECT_URL_SO_1, INCORRECT_URL_SO_2})
-    @DisplayName("Create link with invalid StackOverflow URL")
-    void testCreateLinkWithInvalidURLSO(String url) {
         try {
             Link link = linkFactory.create(USER_ID, url);
             assertNull(link);
