37393c139978b07b41f10bbb6f49370fc3ff3a59
Maria Belikova <mebelikova@edu.hse.ru>
2024-03-08 21:24:45 +0300

    Add Integrations Environment and simple Migrations test

---
+++ scrapper/src/test/java/edu/java/scrapper/integration/IntegrationEnvironment.java
@@ -0,0 +1,63 @@
+package edu.java.scrapper.integration;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.DirectoryResourceAccessor;
+import lombok.SneakyThrows;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@Testcontainers
+public abstract class IntegrationEnvironment {
+    private static final Path MIGRATIONS_PATH = Path.of(System.getProperty("user.dir"))
+        .resolve("src/main/resources/migrations");
+    private static final String CHANGELOG_FILE = "master.xml";
+    public static PostgreSQLContainer<?> POSTGRES;
+    public static Connection connection;
+
+    static {
+        POSTGRES = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("scrapper")
+            .withUsername("postgres")
+            .withPassword("postgres");
+        POSTGRES.start();
+
+        runMigrations(POSTGRES);
+    }
+
+    @SneakyThrows
+    private static void runMigrations(JdbcDatabaseContainer<?> c) {
+        Class.forName("org.postgresql.Driver");
+        try {
+            connection = DriverManager.getConnection(c.getJdbcUrl(), c.getUsername(), c.getPassword());
+            Database database = DatabaseFactory.getInstance()
+                .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Liquibase liquibase = new liquibase.Liquibase(
+                CHANGELOG_FILE,
+                new DirectoryResourceAccessor(MIGRATIONS_PATH),
+                database
+            );
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to execute liquibase migrations", e);
+        }
+    }
+
+    @DynamicPropertySource
+    static void jdbcProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+}
