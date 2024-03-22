7cb50daf0a79c53ef1d122937b6e7fb9b39ab901
Maria Belikova <mebelikova@edu.hse.ru>
2024-03-22 21:36:10 +0300

    Delete commented code and fix codestyle

--- scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -3,7 +3,6 @@ package edu.java.scrapper.configuration;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
